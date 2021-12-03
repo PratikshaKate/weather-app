d20739ac63d4da94b637d8e1c09dc9da2c7ef772
pratiksha kate <pratikshakate@Pratikshas-MacBook-Pro-2.local>
2021-12-03 13:29:42 +0530

    Added  a new text in about

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,10 +16,10 @@ const forecast = (latitude, longitude, callback) => {
     } else if (response.body.error) {
       callback(undefined, "Unable to find the location. Try another search.");
     } else {
-      //console.log(response.body.current);
+      // console.log(response.body.current);
       callback(
         undefined,
-        `The temperature is ${response.body.current.temperature} but it feel like it is ${response.body.current.feelslike}. The weather is currently ${response.body.current.weather_descriptions[0]}`
+        `The temperature is ${response.body.current.temperature} but it feel like it is ${response.body.current.feelslike}. The weather is currently ${response.body.current.weather_descriptions[0]}. The humidity is ${response.body.current.humidity}`
       );
     }
   });
